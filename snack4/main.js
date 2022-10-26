e6c43511b50c2a6d47458fb03728e973f1cccc90
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-10-26 15:42:16 +0200

    snack 4

---
+++ snack4/main.js
@@ -0,0 +1,19 @@
+let arrayInvitati = ["pinco", "pallino", "pippo"];
+
+let nome = prompt("Dimmi il tuo nome");
+
+
+
+let ind = arrayInvitati.indexOf(nome);
+
+if(nome = arrayInvitati[ind]){
+ console.log("Puoi entrare");
+}
+else{
+    console.log("Non sei stato invitato");
+}
+
+
+
+// console.log(arrayInvitati[ind]);
+
